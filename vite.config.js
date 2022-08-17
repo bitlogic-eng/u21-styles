38981bc2040ad89ecc3c06137d368f9fb1e6fb0c
Juan Usubiaga <juan.usubiaga@bitlogic.io>
2022-08-17 17:11:36 -0300

    feat: clean code

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,11 @@
-import { resolve } from "path";
+import path from "path";
 import { defineConfig } from "vite";
-// import copy from "rollup-plugin-copy";
 
-// module.exports = defineConfig({
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, "/"),
-//       name: "U21StylesLib",
-//     },
-//   },
-// });
-
-export default defineConfig({
-  base: "",
-
+module.exports = defineConfig({
   build: {
-    rollupOptions: {
-      input: resolve(__dirname, "index.html"),
-
-      output: {
-        // Prevent vendor.js being created
-        manualChunks: undefined,
-        assetFileNames: "assets/[name].[ext]",
-      },
+    lib: {
+      entry: path.resolve(__dirname, "/main.js"),
+      name: "U21StylesLib",
     },
-    // Prevent vendor.css being created
-    cssCodeSplit: false,
-    // prevent some warnings
-    chunkSizeWarningLimit: 60000,
   },
 });
